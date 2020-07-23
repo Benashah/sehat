fd8f74cd3c117bd1f487e39575e9d945bff1b1d0
Benashah <42694384+Benashah@users.noreply.github.com>
2020-07-23 14:44:06 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,51 +1,125 @@
 package com.example.demo;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.FrameLayout;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.cardview.widget.CardView;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import android.content.Intent;
+import android.icu.text.StringPrepParseException;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public void makeRequest(View view){
+
+
+        EditText testText = (EditText) findViewById(R.id.phoneID);
+        String phoneNumber = testText.getText().toString();
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        String url = "https://sehat.hyderdevelops.ml/users/generateOTP?phone=";
+        url = url.concat(phoneNumber);
+        Log.i("Url",url);
+
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    Toast.makeText(getApplicationContext(),response.toString(),Toast.LENGTH_LONG).show();
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+
+
+
+
+        };
+
+        queue.add(stringRequest);
+    }
+
+
+    public void confrimOtp(View view){
+
+        EditText testText = (EditText) findViewById(R.id.phoneID);
+        String phoneNumber = testText.getText().toString();
+
+        EditText otp = (EditText) findViewById(R.id.otp_rec);
+        String otpValue = otp.getText().toString();
+
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        String url = "https://sehat.hyderdevelops.ml/users/verifyOTP?phone=";
+        url = url.concat(phoneNumber);
+        url = url.concat("&otp=");
+        url = url.concat(otpValue);
+
+        Log.i("Url",url);
+
+
+
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+
+
+            @Override
+            public void onResponse(String response) {
+
+                try{
+                    Toast.makeText(getApplicationContext(),"OTP MATCHED".toString(),Toast.LENGTH_LONG).show();
+                    Intent in = new Intent(MainActivity.this, SecondActivity.class);
+                    in.putExtra("some","data");
+                    startActivity(in);
+
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if(error.networkResponse.statusCode == 401){
+                    Toast.makeText(getApplicationContext(),"Login Denied".toString(),Toast.LENGTH_LONG).show();
+                }
+            }
+        }){
+
+
+
+
+        };
+
+        queue.add(stringRequest);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BottomNavigationView mMainNav = findViewById(R.id.main_nav);
-        mMainNav.setOnNavigationItemSelectedListener(navListener);
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new HomeFragment()).commit();
     }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            Fragment selectedFragment = null;
-
-            switch (menuItem.getItemId()) {
-                case R.id.nav_home:
-                    selectedFragment = new HomeFragment();
-                    break;
-                case R.id.nav_notif:
-                    selectedFragment = new NotificationFragment();
-                    break;
-                case R.id.nav_help:
-                    selectedFragment = new HelpFragment();
-                    break;
-                case R.id.nav_user:
-                    selectedFragment = new UserFragment();
-                    break;
-
-            }
-            getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, selectedFragment).commit();
-            return true;
-        }
-
-    };
 }
